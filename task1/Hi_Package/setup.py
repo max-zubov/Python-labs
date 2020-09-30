35cde20110f8785c32007b78aa56582f07e8203d
max-zubov <62017224+max-zubov@users.noreply.github.com>
2020-09-30 13:49:27 +0300

    Add files via upload

---
+++ task1/Hi_Package/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='hipackage',
+    version='0.1.0',
+    author='Epam Student',
+    author_email='student@example.com',
+    packages=['hipackage'],
+    description='Hi package test build.'
+)
+
